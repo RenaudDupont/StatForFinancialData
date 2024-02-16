5cb49bca7555548192ff7f073b6d4e810604f76a
RenaudDupont <41120287+RenaudDupont@users.noreply.github.com>
2024-02-16 14:15:22 -0500

    working

--- MATH 60633A tp01/main.R
+++ MATH 60633A tp01/main.R
@@ -11,17 +11,12 @@ index_prices = index_prices[start_analysis_date,]
 par(mfrow = c(2,1), ## set 2 charts in 1 row
     cex = 0.6) ## labels are now 60% of their normal size
 
-#plot(index_prices$SP500, main = "SP500 monthly price", col = "blue")
-#plot(index_prices$FTSE100,main = "FTSE100 monthly price", col = "blue")
-
 log_returns_sp500 = CalculateReturns(index_prices$SP500, method = "log")[-1]
 log_returns_fste100 = CalculateReturns(index_prices$FTSE100, method = "log")[-1]
 log_returns = CalculateReturns(index_prices)[-1]
 
 number_of_bins = round(10 *log(length(log_returns)))
 
-#hist(log_returns$SP500, main = "SP500 daily log-returns", col = "blue", breaks = 100)
-#hist(log_returns$FTSE100, main = "FTSE100 daily log-returns", col = "blue", breaks = 100)
 
 # Compute the VaR forecast for the SP500 index
 VaR_forecast_sp500 = f_forecast_var(log_returns_sp500, level = 0.95)
