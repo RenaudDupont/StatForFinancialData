a647f841f4144b901bc7beebb193791ca1362c23
RenaudDupont <41120287+RenaudDupont@users.noreply.github.com>
2024-03-01 10:25:15 -0500

    Final into main

--- MATH 60633A tp01/Code/Simulation.R
+++ MATH 60633A tp01/Code/Simulation.R
@@ -2,8 +2,7 @@ library("here")
 library("xts")
 library("PerformanceAnalytics")
 source(here("Code/f_forecast_var.R"))
-source(here("Code/functions.R"))
-source(here("Tests/UGarchLib.R"))
+source(here("Code/f_uGarch_lib.R"))
 
 
 f_generate_datas <- function(theta, n)
@@ -11,7 +10,8 @@ f_generate_datas <- function(theta, n)
   omega <- theta[1]
   alpha <- theta[2]
   beta <- theta[3]
-  epsilon <- rnorm(n)  # Random shocks from a standard normal distribution
+  epsilon <-
+    rnorm(n)  # Random shocks from a standard normal distribution
   sigma2 <- rep(0, n)  # Conditional variance sigma squared
   y <- rep(0, n)       # Returns y
   
@@ -20,36 +20,62 @@ f_generate_datas <- function(theta, n)
   y[1] <- sqrt(sigma2[1]) * epsilon[1]
   
   # Simulate data using the GARCH(1,1) process
-  for(t in 2:n) {
-    sigma2[t] <- omega + alpha * y[t-1]^2 + beta * sigma2[t-1]
+  for (t in 2:n) {
+    sigma2[t] <- omega + alpha * y[t - 1] ^ 2 + beta * sigma2[t - 1]
     y[t] <- sqrt(sigma2[t]) * epsilon[t]
   }
   y
 }
- 
+
 theta0 <- c(1, 0.1, 0.8)
 
-data = f_generate_datas(theta0, 100000)
+data = f_generate_datas(theta0, 10000)
 
-number_of_bins = round(10 *log(length(data)))
+number_of_bins = round(10 * log(length(data)))
 
-plot(data,type="l", min = -1, max = 1, xlab = "Time", ylab = "Data", main = "Simulated Garch(1,1) data")
+plot(
+  data,
+  type = "l",
+  min = -1,
+  max = 1,
+  xlab = "Time",
+  ylab = "Data",
+  main = "Simulated Garch(1,1) data"
+)
 # Compute the VaR forecast for the SP500 index
 VaR_forecast_sp500 = f_forecast_var(data, level = 0.95)
 VaR_forecast_sp500$GARCH_param
 
 
 # Plot the conditional variances as a time series
-plot(store$objective_values, type = 'l', xlab = "Iteration", ylab = "Objective Function Value",
-     main = "Objective Function Value Over Iterations")
+plot(
+  store$objective_values,
+  type = 'l',
+  xlab = "Iteration",
+  ylab = "Objective Function Value",
+  main = "Objective Function Value Over Iterations"
+)
 
-hist(data, main = "data daily log-returns", col = "blue", breaks = 100)
-abline(v=VaR_forecast_sp500$VaR_Forecast, col="red", lwd=2 )
-abline(v=quantile(data,0.05), col="black", lwd=2 )
-legend("topright", legend=c("VaR from Garch", "VaR from data"), col=c("red", "black"), lty=1:1, cex=0.8)
+hist(data,
+     main = "data daily log-returns",
+     col = "blue",
+     breaks = 100)
+abline(v = VaR_forecast_sp500$VaR_Forecast,
+       col = "red",
+       lwd = 2)
+abline(v = quantile(data, 0.05),
+       col = "black",
+       lwd = 2)
+legend(
+  "topright",
+  legend = c("VaR from Garch", "VaR from data"),
+  col = c("red", "black"),
+  lty = 1:1,
+  cex = 0.8
+)
 
 print("VaR from data ")
-quantile(data,0.05)
+quantile(data, 0.05)
 print("Var from Garch")
 VaR_forecast_sp500$VaR_Forecast
 VaR_forecast_sp500$GARCH_param
@@ -60,20 +86,17 @@ n_sim <- 20
 params <- matrix(0, nrow = n_sim, ncol = 3)
 rugarch <- matrix(0, nrow = n_sim, ncol = 3)
 
-for(i in 1:n_sim) {
+for (i in 1:n_sim) {
   # Simulate data
   data <- f_generate_datas(theta0, 100000)
   # Estimate parameters
   # Compute VaR forecasat
   VaR_forecast <- f_forecast_var(data, level = 0.95)
   # Store the results
-  params[i,] <- VaR_forecast$GARCH_param
-  print(i/n_sim)
+  params[i, ] <- VaR_forecast$GARCH_param
+  print(i / n_sim)
 }
 
-mc_param = c(sum(params[,1])/n_sim, sum(params[,2])/n_sim, sum(params[,3])/n_sim)
+mc_param = c(sum(params[, 1]) / n_sim, sum(params[, 2]) / n_sim, sum(params[, 3]) /
+               n_sim)
 mc_param
-
-
-
-
--- MATH 60633A tp01/Code/f_forecast_var.R
+++ MATH 60633A tp01/Code/f_forecast_var.R
@@ -14,7 +14,7 @@ f_forecast_var <- function(y, level) {
   #   y     : [vector] (T x 1) of observations (log-returns)
   #   level : [scalar] risk level (e.g. 0.95 for a VaR at the 95# risk level)
   #  OUTPUTS
-  #   VaR   : [scalar] VaR forecast 
+  #   VaR   : [scalar] VaR forecast
   #   sig2  : [vector] (T+1 x 1) conditional variances
   #   theta : [vector] GARCH parameters
   #  NOTE
@@ -23,37 +23,52 @@ f_forecast_var <- function(y, level) {
   # Starting values and bounds
   anyNA(y)
   
-  theta0 <- c(0.1* var(y), 0.1, 0.8) 
+  theta0 <- c(0.1 * var(y), 0.1, 0.8)
   LB     <- c(1e-6, 1e-6, 1e-6)
   
   # Stationarity condition
-  A      <- 1 - theta0[3] 
+  A      <- 1 - theta0[3]
   b      <- 1 - theta0[2]
-  UB <- c(Inf, 1-1e-6, 1-1e-6) 
+  UB <- c(Inf, 1 - 1e-6, 1 - 1e-6)
   
   # Run the optimization
-  opt_res = solnp(par = theta0, fun = function(x) f_nll(x,y),
-                  UB = UB, LB = LB,
-                  ineqfun = f_ineq_constraint, ineqLB = 0, ineqUB = 1,   
-                  control = list(trace = 0, maxit = 1000, mins = 1e-8, maxtime = 3600) )
-
+  opt_res = solnp(
+    par = theta0,
+    fun = function(x)
+      f_nll(x, y),
+    UB = UB,
+    LB = LB,
+    ineqfun = f_ineq_constraint,
+    ineqLB = 0,
+    ineqUB = 1,
+    control = list(
+      trace = 0,
+      maxit = 1000,
+      mins = 1e-8,
+      maxtime = 3600
+    )
+  )
+  
   theta <- opt_res$par
   
   # Recompute the conditional variance
   sig2 <- ComputeHtGarch(theta, y)
   
   # Compute the next-day ahead VaR for the Normal model
-  VaR <- qnorm(1-level, sd = sqrt(sig2[length(sig2)])) # VaR at the 95% risk level
+  VaR <-
+    qnorm(1 - level, sd = sqrt(sig2[length(sig2)])) # VaR at the 95% risk level
   
-  out <- list(VaR_Forecast = VaR, 
-              ConditionalVariances = sig2, 
-              GARCH_param = theta)
+  out <- list(
+    VaR_Forecast = VaR,
+    ConditionalVariances = sig2,
+    GARCH_param = theta
+  )
   
   out
 }
 
 f_nll <- function(theta, y) {
-  ### Fonction which computes the negative log likelihood value 
+  ### Fonction which computes the negative log likelihood value
   ### of a GARCH model with Normal errors
   #  INPUTS
   #   theta  : [vector] of parameters
@@ -65,17 +80,24 @@ f_nll <- function(theta, y) {
   # Compute the conditional variance of a GARCH(1,1) model
   sig2 <- ComputeHtGarch(theta, y)
   
-
+  
   sig2 <- sig2[1:T]  # Consider the T values
   
-
-  ll <- sum(dnorm(y, mean = 0, sd = sqrt(sig2), log = TRUE))  # Compute the loglikelihood
   
- 
+  ll <-
+    sum(dnorm(
+      y,
+      mean = 0,
+      sd = sqrt(sig2),
+      log = TRUE
+    ))  # Compute the loglikelihood
+  
+  
   nll <- -ll # Output the negative value
   
-
-  store$objective_values <- c(store$objective_values, ll)  # Store the objective function value
+  
+  store$objective_values <-
+    c(store$objective_values, ll)  # Store the objective function value
   
   nll
 }
@@ -89,11 +111,11 @@ ComputeHtGarch <- function(theta, y) {
   
   sig2 <- numeric(T + 1)
   
-  sig2[1] <- omega /(1-alpha1-beta1)  # Unconditional variance
+  sig2[1] <- omega / (1 - alpha1 - beta1)  # Unconditional variance
   sig2[1] <- var(y)
   
   for (t in 2:(T + 1)) {
-    sig2[t] <- omega + alpha1 * y[t - 1]^2 + beta1 * sig2[t - 1]
+    sig2[t] <- omega + alpha1 * y[t - 1] ^ 2 + beta1 * sig2[t - 1]
   }
   
   return(sig2)
@@ -104,7 +126,7 @@ f_ht_arch <- function(theta, y)  {
   #  INPUTS
   #   x0 : [vector] (3 x 1)
   #   y     : [vector] (T x 1) log-returns
-  #  OUTPUTS 
+  #  OUTPUTS
   #   sig2  : [vector] (T+1 x 1) conditional variances
   
   # Extract the parameters
@@ -121,12 +143,9 @@ f_ht_arch <- function(theta, y)  {
   sig2[1] <- a0 / (1 - a1 - b1)
   
   # Compute conditional variance at each step
-  ### !!! FIXME !!!
   for (t in 2:(T + 1)) {
-    sig2[t] <- a0 + a1 * y[t-1]^2 + b1 * sig2[t-1]
+    sig2[t] <- a0 + a1 * y[t - 1] ^ 2 + b1 * sig2[t - 1]
   }
-  ### !!! FIXME !!!
-  
   
   sig2
 }
